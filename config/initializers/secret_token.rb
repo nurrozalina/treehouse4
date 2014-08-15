714de68766d31f45a0a025cc27f3e3b547a03658
Nur Rozalina <nur.rozalina@gmail.com>
2014-08-15 19:03:32 +0800

    upto rake db-migrate of add_user_id_to_statuses command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treehouse4::Application.config.secret_token = '2b720af940238e999dd1a90bf69ff79e1c342b0a1b5f74762c45ba55ac175891c6e16354e3c8113c2c4dc6813dcc7636b36fc319899581f6587a7fb4b8e09411'
